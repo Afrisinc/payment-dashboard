501491f4c8c1adf6cbfaaf8696583cd36c8d33f8
Elissa Ihimbazwe <freemanofficial21@gmail.com>
2026-07-16 22:33:11 +0200

    fix env entr

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,8 +6,8 @@ ENV_FILE="$DIST_DIR/env-config.js"
 
 echo "Injecting runtime environment variables..."
 
-sed -i "s|__VITE_API_BASE_URL__|${VITE_API_BASE_URL:-}|g" "$ENV_FILE"
-sed -i "s|__VITE_AUTH_UI_URL__|${VITE_AUTH_UI_URL:-}|g" "$ENV_FILE"
+sed -i "s|__VITE_API_BASE_URL__|${VITE_API_BASE_URL:-__VITE_API_BASE_URL__}|g" "$ENV_FILE"
+sed -i "s|__VITE_AUTH_UI_URL__|${VITE_AUTH_UI_URL:-__VITE_AUTH_UI_URL__}|g" "$ENV_FILE"
 
 echo "Environment injection complete:"
 cat "$ENV_FILE"
